0e1929fb1cb76dd89d98f25f5e4e0ddf25eef35e
AdrienBertaud <48830443+AdrienBertaud@users.noreply.github.com>
2020-04-22 14:31:44 +0200

    Fix expected value error in newton unit test

--- UnitTests/NewtonUnitTest.py
+++ UnitTests/NewtonUnitTest.py
@@ -13,24 +13,24 @@ reload(Newton) # Relaod the module, in case it has changed
 from Newton import newton
 
 class TestNewton(unittest.TestCase):
-        
+
     def test_identity(self):
         self.assertEqual(newton(lambda x:x, lambda x:1, 1, 1, False), 0.0)
-        
+
     def test_const(self):
         self.assertEqual(newton(lambda x:1, lambda x:0, 1.3, 1, False), 1.3)
-        
+
     def test_x2_1_iter(self):
         self.assertEqual(newton(lambda x:x**2, lambda x:2*x, 1, 1, False), 0.5)
-        
+
     def test_x2_10_iter(self):
         self.assertAlmostEqual(newton(lambda x:x**2, lambda x:2*x, 1, 10, False), 0.0, places =2)
-        
+
     def test_verbose(self):
         self.assertEqual(newton(lambda x:x**2, lambda x:2*x, 1, 2), 0.25)
-        
+
     def test_plot(self):
-        self.assertEqual(newton(lambda x:x**2, lambda x:2*x, 1, N=5, plot=True), 0.25)
-        
+        self.assertAlmostEqual(newton(lambda x:x**2, lambda x:2*x, 1, N=5, plot=True), 0.03, places =2)
+
 if __name__ == '__main__':
     unittest.main()
