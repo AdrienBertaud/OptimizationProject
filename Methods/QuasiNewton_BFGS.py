4cef5833a8b16591060b52170d3c268e1903fc12
Yanis-epfl <62603306+Yanis-epfl@users.noreply.github.com>
2020-05-07 10:59:59 +0200

    Complete BGFS update

--- Methods/QuasiNewton_BFGS.py
+++ Methods/QuasiNewton_BFGS.py
@@ -8,20 +8,37 @@ Created on Thu Apr 30 10:19:31 2020
 import numpy as np
 import numpy.linalg as linalg
 
-def BFGS(f, df, x0, x1, N):
+def BFGS(f, df, x0, N):
     d = x0.shape[0]
     
-    s = []
+    sigma = []
     y = []
+    x = []
+    f_ = []
     
-    s0 = x1 - x0
-    y0 = df(x1) - df(x0)
+    H = np.eye(d)
+    df0 = df(x0)
     
-    s.append(s0)
-    y.append(y0)
-    
-    gamma = np.dot(y0, y0)/(np.dot(y0, s0))
-    B = gamma * np.eye(d)
-    
-    #Pas fini, à continuer selon les notes de cours ou les articles choisis
+    for i in range(N):
+        x.append(x0)
+        f_.append(f(x0))
+
+        x1 = x0 - np.dot(H, df0)
+        df1 = df(x1)
+        sigma.append(x1 - x0)
+        y.append(df1 - df0)
+
+        st = sigma[-1]
+        yt = y[-1]
+
+        Et = 1/(yt.T @ st) * (- H @ yt @ st.T - st @ yt.T @ H + (1+(yt.T @ H @ yt)/(yt.T @ st)) * st @ st.T)
+        H = H + Et
+
+        x0 = x1
+        df0 = df1
+
+    x.append(x0)
+    f_.append(f(x0))
+
+    return x, f_
         
