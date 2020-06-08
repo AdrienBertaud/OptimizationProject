033dbcced6373e084a99c6ac89ba5f1bf583558a
AdrienBertaud <bertaud.adrien@gmail.com>
2020-06-08 12:08:32 +0200

    remove test value for epochs

--- utils/trainer.py
+++ utils/trainer.py
@@ -14,7 +14,7 @@ def train(model, loss_function, optimizer, optimizer_name, data_loader, batch_si
     n_batchs = len(data_loader)
     eval_frequency=400//n_batchs
     loss_condition = 1e-2
-    max_epochs=1//n_batchs
+    max_epochs=10000//n_batchs
 
     print("optimizer.type = ", optimizer_name)
 
