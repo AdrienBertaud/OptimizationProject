299242062ea55f370a1aa62aaa49bee7fde2ef54
AdrienBertaud <bertaud.adrien@gmail.com>
2020-06-09 17:06:40 +0200

    Update plot.py

--- plot.py
+++ plot.py
@@ -7,8 +7,8 @@ import math
 
 
 COLOR_LIST = ['C0', 'C1', 'C2', 'C3', 'C4', 'C5', 'C6', 'C6', 'C7', 'C7', 'C8', 'C9']
+SAVE_DIRECTORY = "graph"
 
-directory_to_save_img = "graph"
 
 def get_sharpness_ub(learning_rate):
     return 2/learning_rate
@@ -25,7 +25,7 @@ def get_nonuniformity_ub(learning_rate, data_size = 1000, batch_size = 1000):
 
 def plot_batch(df, batch_size = 10, data_size = 1000, ymax = 200, eval_algo='sharpness'):
     '''
-    plot sharpness for a fixed batch size but different learning rate
+    plot sharpness for a fixed batch size but different learning rates
     '''
 
     if batch_size == 'all':
@@ -39,7 +39,7 @@ def plot_batch(df, batch_size = 10, data_size = 1000, ymax = 200, eval_algo='sha
 
         data = df[(df['optimizer'] == optimizer) & (df['lr'] == learning_rate)]
 
-        plt.scatter(data['lr'], data[(eval_algo + ' train')],
+        plt.scatter(data['lr'], data[(eval_algo)],
                     c = COLOR_LIST[i], label = optimizer+', lr='+str(learning_rate))
         plt.hlines(get_nonuniformity_ub(learning_rate, data_size = data_size, batch_size = batch_size), 0, get_sharpness_ub(learning_rate),
                    color = COLOR_LIST[i], linestyles = '--', linewidth = 3)
@@ -54,7 +54,7 @@ def plot_batch(df, batch_size = 10, data_size = 1000, ymax = 200, eval_algo='sha
     max_lr =df_bs['lr'].max()
     plt.xlim(0, get_sharpness_ub(max_lr)+1)
     plt.legend(loc = 'best')
-    plt.savefig((directory_to_save_img + '/' 'Sharpness vs Nonuniformity for different learning rate bs '+str(batch_size)+'.png'))
+    plt.savefig((SAVE_DIRECTORY + '/' 'Sharpness vs Nonuniformity for different learning rate bs '+str(batch_size)+'.png'))
     plt.title('batch size = %d'%(batch_size))
     plt.show()
 
@@ -62,7 +62,6 @@ def plot_batch(df, batch_size = 10, data_size = 1000, ymax = 200, eval_algo='sha
 def plot_vs_learning_rate(df, batch_size = 'all', eval_algo='sharpness'):
     '''
     plot sharpness against learning rate with one or many batch size for different algorithms
-
     '''
     if batch_size == 'all':
         df_plot = df.groupby(['optimizer'])
@@ -74,11 +73,11 @@ def plot_vs_learning_rate(df, batch_size = 'all', eval_algo='sharpness'):
         plt.title('batch size = %d'%(batch_size))
 
     for optimizer, values in df_plot:
-        plt.scatter(values['lr'], values[(eval_algo + ' train')], label = optimizer)#, s = values['batch size'])
+        plt.scatter(values['lr'], values[(eval_algo)], label = optimizer)#, s = values['batch size'])
 
     lr_max = df_plot['lr'].max()
 
-    ymax = df_plot[(eval_algo + ' train')].max().max() * 5
+    ymax = df_plot[(eval_algo)].max().max() * 5
     if math.isnan(ymax) :
         return
 
@@ -90,7 +89,7 @@ def plot_vs_learning_rate(df, batch_size = 'all', eval_algo='sharpness'):
     plt.ylim(0, ymax)
     plt.xlabel('learning rate', fontsize = 12)
     plt.ylabel(eval_algo, fontsize = 12)
-    plt.savefig((directory_to_save_img + '/' 'sharpness vs learning rate.png'))
+    plt.savefig((SAVE_DIRECTORY + '/' 'sharpness vs learning rate.png'))
     plt.legend()
 
     plt.show()
@@ -108,7 +107,7 @@ def plot_vs_batch_size(df, eval_algo='sharpness', optim_1 = 'adagrad', optim_2 =
     df_sgd = df[(df['optimizer'] == optim_1) | (df['optimizer'] == optim_2)]
     df_plot = pd.DataFrame(columns = ['lr', 'bs', eval_algo])
     for lr_bs, value in df_sgd.groupby(['lr', 'batch size']):
-        df_plot = df_plot.append({'lr': lr_bs[0], 'bs': int(lr_bs[1]), eval_algo: round(value.median()[(eval_algo + ' train')],1)}, ignore_index=True)
+        df_plot = df_plot.append({'lr': lr_bs[0], 'bs': int(lr_bs[1]), eval_algo: round(value.median()[(eval_algo)],1)}, ignore_index=True)
 
     df_plot['bs'] = df_plot['bs'].astype(int)
     bs_order = [str(i) for i in sorted(list(set(df_plot['bs'])))]
@@ -125,7 +124,7 @@ def plot_vs_batch_size(df, eval_algo='sharpness', optim_1 = 'adagrad', optim_2 =
     plt.ylabel(eval_algo, fontsize = 12)
     plt.legend()
     fig_name = (optim_2 + ' ' + eval_algo + ' vs batch size')
-    plt.savefig((directory_to_save_img + '/' + fig_name + '.png'))
+    plt.savefig((SAVE_DIRECTORY + '/' + fig_name + '.png'))
     plt.title(fig_name)
     plt.show()
 
@@ -143,12 +142,12 @@ def main():
     df = pd.read_csv(results_file, sep = ',')
 
     # create directory to save images
-    if not os.path.exists(directory_to_save_img):
-        os.makedirs(directory_to_save_img)
+    if not os.path.exists(SAVE_DIRECTORY):
+        os.makedirs(SAVE_DIRECTORY)
 
     # define the parameters from which we want to plot graphs
     batch_size_list = [1000, 100, 25, 10, 5, 'all']
-    eval_algo_list = ['sharpness','non uniformity']
+    eval_algo_list = ['sharpness train','non uniformity train', 'duration']
 
     # filter the evaluations, so as to use only the relevant ones
     df_filtered = df[(df['train loss'] <= 1e-3) &
