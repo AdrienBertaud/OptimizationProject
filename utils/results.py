f3711a7500497309fbf163a5e99b74636d8ebfd2
AdrienBertaud <bertaud.adrien@gmail.com>
2020-06-11 17:20:23 +0200

    plot lbgs

--- utils/results.py
+++ utils/results.py
@@ -48,7 +48,7 @@ def save_results_to_csv(optimizer_name, \
                     'train accuracy': round(train_accuracy,1),
                     'test loss': round(test_loss,5),
                     'test accuracy': round(test_accuracy,1),
-                    'sharpness': round(sharpness_train,1),
+                    'sharpness': round(sharpness_train,2),
                     'non uniformity': round(non_uniformity_train),
                     'sharpness test': round(sharpness_test),
                     'non uniformity test': round(non_uniformity_test)},
